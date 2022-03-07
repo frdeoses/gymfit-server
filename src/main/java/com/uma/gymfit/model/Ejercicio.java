c54810e17c45fe69e70f5599644beca210b583ba
frdeoses <fdoses@outlook.es>
2022-03-07 21:33:50 +0100

    Update Ejercicio

--- src/main/java/com/uma/gymfit/model/Ejercicio.java
+++ src/main/java/com/uma/gymfit/model/Ejercicio.java
@@ -1,2 +1,33 @@
-package com.uma.gymfit.model;public class Ejercicio {
+package com.uma.gymfit.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Ejercicio {
+
+    @Id
+    private String id;
+
+    private String tipoEjercicio;
+
+    private int numRepeticiones;
+
+    private int numSeries;
+
+    private String zonaEjercitada;
+
+    private String explicacion;
+
+    private int like;
+
+    private List<PesoTrabajado> pesosTrabajados;
+    
+    private Maquina maquina;
+
 }
