d4ad9082a050a81df44fa36f637ae77dae9cbe77
frdeoses <fdoses@outlook.es>
2022-03-12 13:45:21 +0100

    Rename Class

--- src/main/java/com/uma/gymfit/repository/IRepositorioUsuario.java
+++ src/main/java/com/uma/gymfit/repository/IRepositorioUsuario.java
@@ -3,5 +3,5 @@ package com.uma.gymfit.repository;
 import com.uma.gymfit.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface IUserRepository extends MongoRepository<Usuario,String> {
+public interface IRepositorioUsuario extends MongoRepository<Usuario,String> {
 }
