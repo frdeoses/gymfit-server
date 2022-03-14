42b1074c33ac2e41a4847f06bb71a75f035de198
frdeoses <fdoses@outlook.es>
2022-03-14 20:23:47 +0100

    Creeate "findByIdTablaEntrenamiento" and "findByIdUser"

--- src/main/java/com/uma/gymfit/repository/IRepositorioTabla.java
+++ src/main/java/com/uma/gymfit/repository/IRepositorioTabla.java
@@ -3,5 +3,10 @@ package com.uma.gymfit.repository;
 import com.uma.gymfit.model.TablaEntrenamiento;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface IRepositorioTabla extends MongoRepository<TablaEntrenamiento,String> {
+import java.util.Optional;
+
+public interface IRepositorioTabla<T extends TablaEntrenamiento>
+        extends MongoRepository<TablaEntrenamiento,String> {
+
+    Optional<T> findByIdTablaEntrenamiento(String id);
 }
--- src/main/java/com/uma/gymfit/repository/IRepositorioUsuario.java
+++ src/main/java/com/uma/gymfit/repository/IRepositorioUsuario.java
@@ -3,5 +3,9 @@ package com.uma.gymfit.repository;
 import com.uma.gymfit.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface IRepositorioUsuario extends MongoRepository<Usuario,String> {
+import java.util.Optional;
+
+public interface IRepositorioUsuario<U extends Usuario> extends MongoRepository<Usuario,String> {
+
+    Optional<U> findByIdUser(String id);
 }
