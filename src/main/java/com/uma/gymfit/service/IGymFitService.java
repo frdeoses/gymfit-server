68a4396948a0fe66897c6d2e7ae4fc30451eb796
frdeoses <fdoses@outlook.es>
2022-03-14 20:13:57 +0100

    Create interface and service

---
+++ src/main/java/com/uma/gymfit/service/IGymFitService.java
@@ -0,0 +1,9 @@
+package com.uma.gymfit.service;
+
+import com.uma.gymfit.model.TablaEntrenamiento;
+import com.uma.gymfit.model.Usuario;
+
+import java.net.http.HttpResponse;
+
+public interface IGymFitService<T extends TablaEntrenamiento, U extends Usuario> {
+}
---
+++ src/main/java/com/uma/gymfit/service/impl/GymFitService.java
@@ -0,0 +1,9 @@
+package com.uma.gymfit.service.impl;
+
+import com.uma.gymfit.model.TablaEntrenamiento;
+import com.uma.gymfit.model.Usuario;
+import com.uma.gymfit.service.IGymFitService;
+
+public class GymFitService<T extends TablaEntrenamiento, U extends Usuario>
+        implements IGymFitService {
+}
