501994f0a070995b52647f28ade1990aa42138db
frdeoses <fdoses@outlook.es>
2022-09-06 19:35:20 +0200

    Update model ResponseHTTP

--- src/main/java/com/uma/gymfit/model/ResponseHTTP.java
+++ src/main/java/com/uma/gymfit/model/ResponseHTTP.java
@@ -1,4 +1,24 @@
 package com.uma.gymfit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class ResponseHTTP {
+
+    private int code;
+
+    private String response;
+
+    private Object body;
+
+    private String error;
+
+
 }
