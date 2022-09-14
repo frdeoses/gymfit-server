f85c1a65ab22e1b0b70d729d60928ffb37bd7a7f
frdeoses <fdoses@outlook.es>
2022-09-14 19:41:12 +0200

    Rename models

---
+++ user-server/src/main/java/com/uma/gymfit/user/model/User.java
@@ -0,0 +1,58 @@
+package com.uma.gymfit.user.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Document(value = "User")
+@Data
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    private String id;
+
+    @JsonProperty(required = true)
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @JsonProperty(required = true)
+    @NotNull
+    @NotBlank
+    private String surname;
+
+    @Indexed(unique = true)
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+
+    @JsonProperty(required = true)
+    @NotNull
+    private long birthdate;
+
+    @JsonProperty(required = true)
+    @NotNull
+    private long registrationDate;
+
+    @JsonProperty(required = true)
+    private double altura;
+
+    private List<Weight> pesoUsuario;
+
+    private List<FatPercentage> porcentajeGrasa;
+
+}
