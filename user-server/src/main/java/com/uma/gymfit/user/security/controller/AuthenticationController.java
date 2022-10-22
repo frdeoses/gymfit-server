d8419f8a05b61ff954594130060a3abc51e39ee3
frdeoses <fdoses@outlook.es>
2022-10-22 16:03:12 +0200

    Create controller

---
+++ user-server/src/main/java/com/uma/gymfit/user/security/controller/AuthenticationController.java
@@ -0,0 +1,53 @@
+package com.uma.gymfit.user.security.controller;
+
+import com.uma.gymfit.user.security.config.JwtUtils;
+import com.uma.gymfit.user.security.model.JwtRequest;
+import com.uma.gymfit.user.security.model.JwtResponse;
+import com.uma.gymfit.user.security.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generarToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            autenticar(jwtRequest.getUsername(), jwtRequest.getPassword());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new Exception("Usuario no encontrado");
+        }
+
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = this.jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void autenticar(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException exception) {
+            throw new Exception("USUARIO DESHABILITADO " + exception.getMessage());
+        } catch (BadCredentialsException e) {
+            throw new Exception("Credenciales invalidas " + e.getMessage());
+        }
+    }
+}
