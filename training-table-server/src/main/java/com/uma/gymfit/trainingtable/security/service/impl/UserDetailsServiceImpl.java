6f0bbd61c8b40090d2da74a201e3cbfaa9cf60d6
frdeoses <fdoses@outlook.es>
2022-12-08 12:06:49 +0100

    add security config

---
+++ training-table-server/src/main/java/com/uma/gymfit/trainingtable/security/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.uma.gymfit.trainingtable.security.service.impl;
+
+import com.uma.gymfit.trainingtable.model.user.User;
+import com.uma.gymfit.trainingtable.repository.IUserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+
+        if(user == null){
+            log.error("Usuario no encontrado...");
+            throw  new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+        return user;
+    }
+}
