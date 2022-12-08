5cd249939ca08999ed749d199f2930410e7ac6f8
frdeoses <fdoses@outlook.es>
2022-12-08 12:09:21 +0100

    Update folder and add url 'generate token'

--- calendar-server/src/main/java/com/uma/gymfit/calendar/repository/ICalendarRepository.java
+++ calendar-server/src/main/java/com/uma/gymfit/calendar/repository/ICalendarRepository.java
@@ -1,7 +1,7 @@
 package com.uma.gymfit.calendar.repository;
 
 
-import com.uma.gymfit.calendar.model.Calendar;
+import com.uma.gymfit.calendar.model.calendar.Calendar;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ICalendarRepository extends MongoRepository<Calendar,String> {
