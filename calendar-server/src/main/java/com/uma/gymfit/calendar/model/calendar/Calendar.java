92dda3a8e63f0bbdaf2f8879ff601e836d9abe5c
frdeoses <fdoses@outlook.es>
2022-12-16 18:42:54 +0100

    add var published in Calendar entity

--- calendar-server/src/main/java/com/uma/gymfit/calendar/model/calendar/Calendar.java
+++ calendar-server/src/main/java/com/uma/gymfit/calendar/model/calendar/Calendar.java
@@ -1,17 +1,27 @@
 package com.uma.gymfit.calendar.model.calendar;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class Calendar {
 
+    @Id
     private String id;
 
+    @JsonProperty(required = true)
+    @NotNull
+    @NotBlank
     private String title;
 
     private String description;
+
+    private boolean published;
 
     private List<Comment> comments;
 
