fb9bfbc92f00a7627951d7350336770087baff4a
frdeoses <fdoses@outlook.es>
2023-02-26 19:53:25 +0100

    add new urls

--- training-table-server/src/main/java/com/uma/gymfit/trainingtable/utils/Literals.java
+++ training-table-server/src/main/java/com/uma/gymfit/trainingtable/utils/Literals.java
@@ -6,15 +6,23 @@ public class Literals {
 
     public static final String TRAINING_TABLES = "/training-tables";
 
-    public static final String GYM_MACHINES = "/gym-machines";
+    public static final String TYPE_TRAINING = "/training-tables/type-training";
+
+    public static final String GYM_MACHINES = "/training-tables/gym-machines";
+
+    public static final String TRAININGS = "/training-tables/trainings";
 
     public static final String TRAINING_TABLE_ID = "/training-tables/{idTrainingTable}";
 
-    public static final String GYM_MACHINE_ID = "/gym-machines/{idGymMachine}";
+    public static final String GYM_MACHINE_ID = "/training-tables/gym-machines/{idGymMachine}";
+
+    public static final String TRAINING_ID = "/training-tables/trainings/{idTraining}";
 
     public static final String TRAINING_TABLE = "/training-table";
 
-    public static final String GYM_MACHINE = "/gym-machine";
+    public static final String GYM_MACHINE = "/training-tables/gym-machine";
+
+    public static final String TRAINING = "/training-tables/training";
 
     public static final String GENERATE_TOKEN = "/api/gymfit/training-tables/generate-token";
 
