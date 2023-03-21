c2de72a62d700ae5e58d34aab358ce403a21186d
frdeoses <fdoses@outlook.es>
2023-03-21 19:28:23 +0100

    add new url

--- calendar-server/src/main/java/com/uma/gymfit/calendar/utils/Literals.java
+++ calendar-server/src/main/java/com/uma/gymfit/calendar/utils/Literals.java
@@ -6,6 +6,8 @@ public class Literals {
 
     public static final String CALENDARS = "/calendars";
 
+    public static final String CALENDARS_PUBLISHED = "/calendars/published";
+
     public static final String CALENDAR_ID = "/calendars/{idCalendar}";
 
     public static final String CALENDAR = "/calendar";
