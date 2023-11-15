7382b81ac9c8c5d7ed43ebaf5c6edb0e0835e344
frdeoses <frdeosesfuentelu@gmail.com>
2023-11-15 19:24:05 +0100

    Refactor code

--- training-table-server/src/main/java/com/uma/gymfit/trainingtable/controller/GymMachineController.java
+++ training-table-server/src/main/java/com/uma/gymfit/trainingtable/controller/GymMachineController.java
@@ -2,13 +2,7 @@ package com.uma.gymfit.trainingtable.controller;
 
 import com.uma.gymfit.trainingtable.model.ResponseHTTP;
 import com.uma.gymfit.trainingtable.model.training.GymMachine;
-import com.uma.gymfit.trainingtable.model.training.Training;
-import com.uma.gymfit.trainingtable.model.training.TrainingTable;
-import com.uma.gymfit.trainingtable.model.training.TrainingType;
-import com.uma.gymfit.trainingtable.model.user.User;
 import com.uma.gymfit.trainingtable.service.IGymMachineService;
-import com.uma.gymfit.trainingtable.service.ITrainingService;
-import com.uma.gymfit.trainingtable.service.ITrainingTableService;
 import com.uma.gymfit.trainingtable.utils.Literals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,103 +15,17 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT})
 @RequestMapping(Literals.API)
-public class TrainingTableController {
-    
-    @Autowired
-    private ITrainingTableService trainingTableService;
+public class GymMachineController {
 
     @Autowired
     private IGymMachineService gymMachineService;
 
-    @Autowired
-    private ITrainingService trainingService;
-
-
-    @GetMapping(Literals.TRAINING_TABLES)
-    public ResponseEntity<List<TrainingTable>> allTrainingTable() {
-        return new ResponseEntity<>(trainingTableService.allTrainingTable(), HttpStatus.OK);
-    }
-
-    @GetMapping(Literals.TYPE_TRAINING)
-    public ResponseEntity<TrainingType[]> allTrainingType() {
-        return new ResponseEntity<>(trainingService.trainingType(), HttpStatus.OK);
-    }
 
     @GetMapping(Literals.GYM_MACHINES)
     public ResponseEntity<List<GymMachine>> allGymMachine() {
         return new ResponseEntity<>(gymMachineService.allGymMachine(), HttpStatus.OK);
     }
 
-    @GetMapping(Literals.TRAININGS)
-    public ResponseEntity<List<Training>> allTraining() {
-        return new ResponseEntity<>(trainingService.allTraining(), HttpStatus.OK);
-    }
-
-
-    @GetMapping(Literals.TRAINING_TABLE_ID)
-    public ResponseEntity<TrainingTable> findTrainingTable(@PathVariable String idTrainingTable) {
-
-        TrainingTable trainingTable;
-        try {
-            trainingTable = trainingTableService.findTrainingTable(idTrainingTable);
-            return new ResponseEntity(trainingTable, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTrainingTable, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping(Literals.TRAINING_TABLE_TYPE)
-    public ResponseEntity<List<TrainingTable>> findByTrainingType(@RequestParam String typeTraining, @RequestParam String idUser) {
-
-        List<TrainingTable> trainingTables;
-        try {
-            trainingTables = trainingTableService.findByTrainingType(typeTraining, idUser);
-            return new ResponseEntity(trainingTables, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), typeTraining, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping(Literals.TRAINING_TABLE_USER)
-    public ResponseEntity<List<TrainingTable>> findByUser(@RequestBody User user) {
-
-        List<TrainingTable> trainingTables;
-        try {
-            trainingTables = trainingTableService.findByUser(user);
-            return new ResponseEntity(trainingTables, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), user, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping(Literals.TRAININGS_BY_USER)
-    public ResponseEntity<List<Training>> listTrainingByUser(@RequestBody User user) {
-
-        List<Training> trainingList;
-        try {
-            trainingList = trainingService.findTrainingsByUser(user);
-            return new ResponseEntity(trainingList, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), user, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping(Literals.TRAININGS_BY_TYPE_TRAINING)
-    public ResponseEntity<List<Training>> findTrainingByTrainingType(@RequestParam String typeTraining, @RequestParam String idUser) {
-
-        List<Training> trainingList;
-        try {
-            trainingList = trainingService.findTrainingsByTrainingType(typeTraining, idUser);
-            return new ResponseEntity(trainingList, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), typeTraining, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
     @GetMapping(Literals.GYM_MACHINE_ID)
     public ResponseEntity<GymMachine> findGymMachine(@PathVariable String idGymMachine) {
@@ -132,32 +40,6 @@ public class TrainingTableController {
         }
     }
 
-    @GetMapping(Literals.TRAINING_ID)
-    public ResponseEntity<Training> findTraining(@PathVariable String idTraining) {
-
-        Training training;
-        try {
-            training = trainingService.findTraining(idTraining);
-            return new ResponseEntity(training, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTraining, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    @PostMapping(Literals.TRAINING_TABLE)
-    public ResponseEntity<ResponseHTTP> createTrainingTable(@Validated @RequestBody TrainingTable trainingTable) {
-
-        try {
-            trainingTableService.createTrainingTable(trainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.CREATED.value(), HttpStatus.CREATED.toString(), trainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.CREATED);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), trainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
     @PostMapping(Literals.GYM_MACHINE)
     public ResponseEntity<ResponseHTTP> createGymMachine(@Validated @RequestBody GymMachine gymMachine) {
@@ -172,46 +54,6 @@ public class TrainingTableController {
         }
     }
 
-    @PostMapping(Literals.TRAINING)
-    public ResponseEntity<ResponseHTTP> createTraining(@Validated @RequestBody Training training) {
-
-        try {
-            trainingService.createTraining(training);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.CREATED.value(), HttpStatus.CREATED.toString(), training, null);
-            return new ResponseEntity<>(res, HttpStatus.CREATED);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), training, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PutMapping(Literals.TRAINING_TABLE)
-    public ResponseEntity<ResponseHTTP> updateTrainingTable(@RequestBody TrainingTable trainingTable) {
-
-        try {
-            trainingTableService.updateTrainingTable(trainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), trainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), trainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @PutMapping(Literals.TRAINING)
-    public ResponseEntity<ResponseHTTP> updateTraining(@RequestBody Training training) {
-
-        try {
-            trainingService.updateTraining(training);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), training, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), training, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
 
     @PutMapping(Literals.GYM_MACHINE)
     public ResponseEntity<ResponseHTTP> updateGymMachine(@RequestBody GymMachine gymMachine) {
@@ -227,19 +69,6 @@ public class TrainingTableController {
 
     }
 
-    @DeleteMapping(Literals.TRAINING_TABLE_ID)
-    public ResponseEntity<ResponseHTTP> deleteTrainingTable(@PathVariable String idTrainingTable) {
-
-        try {
-            trainingTableService.deleteTrainingTable(idTrainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), idTrainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTrainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
 
     @DeleteMapping(Literals.GYM_MACHINE_ID)
     public ResponseEntity<ResponseHTTP> deleteGymMachine(@PathVariable String idGymMachine) {
@@ -255,15 +84,15 @@ public class TrainingTableController {
 
     }
 
-    @DeleteMapping(Literals.TRAINING_ID)
-    public ResponseEntity<ResponseHTTP> deleteTraining(@PathVariable String idTraining) {
+    @DeleteMapping(Literals.GYM_MACHINE_EMPTY_REPOSITORY)
+    public ResponseEntity<ResponseHTTP> deleteAllGymMachine() {
 
         try {
-            trainingService.deleteTraining(idTraining);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), idTraining, null);
+            gymMachineService.deleteAllGymMachine();
+            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), null, null);
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTraining, e.getMessage());
+            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), null, e.getMessage());
             return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
--- training-table-server/src/main/java/com/uma/gymfit/trainingtable/controller/TrainingController.java
+++ training-table-server/src/main/java/com/uma/gymfit/trainingtable/controller/TrainingController.java
@@ -1,14 +1,10 @@
 package com.uma.gymfit.trainingtable.controller;
 
 import com.uma.gymfit.trainingtable.model.ResponseHTTP;
-import com.uma.gymfit.trainingtable.model.training.GymMachine;
 import com.uma.gymfit.trainingtable.model.training.Training;
-import com.uma.gymfit.trainingtable.model.training.TrainingTable;
 import com.uma.gymfit.trainingtable.model.training.TrainingType;
 import com.uma.gymfit.trainingtable.model.user.User;
-import com.uma.gymfit.trainingtable.service.IGymMachineService;
 import com.uma.gymfit.trainingtable.service.ITrainingService;
-import com.uma.gymfit.trainingtable.service.ITrainingTableService;
 import com.uma.gymfit.trainingtable.utils.Literals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,76 +17,19 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT})
 @RequestMapping(Literals.API)
-public class TrainingTableController {
-    
-    @Autowired
-    private ITrainingTableService trainingTableService;
-
-    @Autowired
-    private IGymMachineService gymMachineService;
+public class TrainingController {
 
     @Autowired
     private ITrainingService trainingService;
-
-
-    @GetMapping(Literals.TRAINING_TABLES)
-    public ResponseEntity<List<TrainingTable>> allTrainingTable() {
-        return new ResponseEntity<>(trainingTableService.allTrainingTable(), HttpStatus.OK);
-    }
-
+    
     @GetMapping(Literals.TYPE_TRAINING)
     public ResponseEntity<TrainingType[]> allTrainingType() {
         return new ResponseEntity<>(trainingService.trainingType(), HttpStatus.OK);
     }
 
-    @GetMapping(Literals.GYM_MACHINES)
-    public ResponseEntity<List<GymMachine>> allGymMachine() {
-        return new ResponseEntity<>(gymMachineService.allGymMachine(), HttpStatus.OK);
-    }
-
     @GetMapping(Literals.TRAININGS)
     public ResponseEntity<List<Training>> allTraining() {
         return new ResponseEntity<>(trainingService.allTraining(), HttpStatus.OK);
-    }
-
-
-    @GetMapping(Literals.TRAINING_TABLE_ID)
-    public ResponseEntity<TrainingTable> findTrainingTable(@PathVariable String idTrainingTable) {
-
-        TrainingTable trainingTable;
-        try {
-            trainingTable = trainingTableService.findTrainingTable(idTrainingTable);
-            return new ResponseEntity(trainingTable, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTrainingTable, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping(Literals.TRAINING_TABLE_TYPE)
-    public ResponseEntity<List<TrainingTable>> findByTrainingType(@RequestParam String typeTraining, @RequestParam String idUser) {
-
-        List<TrainingTable> trainingTables;
-        try {
-            trainingTables = trainingTableService.findByTrainingType(typeTraining, idUser);
-            return new ResponseEntity(trainingTables, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), typeTraining, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping(Literals.TRAINING_TABLE_USER)
-    public ResponseEntity<List<TrainingTable>> findByUser(@RequestBody User user) {
-
-        List<TrainingTable> trainingTables;
-        try {
-            trainingTables = trainingTableService.findByUser(user);
-            return new ResponseEntity(trainingTables, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), user, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     @PostMapping(Literals.TRAININGS_BY_USER)
@@ -119,19 +58,6 @@ public class TrainingTableController {
         }
     }
 
-    @GetMapping(Literals.GYM_MACHINE_ID)
-    public ResponseEntity<GymMachine> findGymMachine(@PathVariable String idGymMachine) {
-
-        GymMachine gymMachine;
-        try {
-            gymMachine = gymMachineService.findGymMachine(idGymMachine);
-            return new ResponseEntity(gymMachine, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idGymMachine, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
     @GetMapping(Literals.TRAINING_ID)
     public ResponseEntity<Training> findTraining(@PathVariable String idTraining) {
 
@@ -142,33 +68,6 @@ public class TrainingTableController {
         } catch (Exception e) {
             ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTraining, e.getMessage());
             return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    @PostMapping(Literals.TRAINING_TABLE)
-    public ResponseEntity<ResponseHTTP> createTrainingTable(@Validated @RequestBody TrainingTable trainingTable) {
-
-        try {
-            trainingTableService.createTrainingTable(trainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.CREATED.value(), HttpStatus.CREATED.toString(), trainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.CREATED);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), trainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping(Literals.GYM_MACHINE)
-    public ResponseEntity<ResponseHTTP> createGymMachine(@Validated @RequestBody GymMachine gymMachine) {
-
-        try {
-            gymMachineService.createGymMachine(gymMachine);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.CREATED.value(), HttpStatus.CREATED.toString(), gymMachine, null);
-            return new ResponseEntity<>(res, HttpStatus.CREATED);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), gymMachine, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -185,20 +84,6 @@ public class TrainingTableController {
         }
     }
 
-    @PutMapping(Literals.TRAINING_TABLE)
-    public ResponseEntity<ResponseHTTP> updateTrainingTable(@RequestBody TrainingTable trainingTable) {
-
-        try {
-            trainingTableService.updateTrainingTable(trainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), trainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), trainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
     @PutMapping(Literals.TRAINING)
     public ResponseEntity<ResponseHTTP> updateTraining(@RequestBody Training training) {
 
@@ -208,48 +93,6 @@ public class TrainingTableController {
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (Exception e) {
             ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), training, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @PutMapping(Literals.GYM_MACHINE)
-    public ResponseEntity<ResponseHTTP> updateGymMachine(@RequestBody GymMachine gymMachine) {
-
-        try {
-            gymMachineService.updateGymMachine(gymMachine);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), gymMachine, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), gymMachine, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @DeleteMapping(Literals.TRAINING_TABLE_ID)
-    public ResponseEntity<ResponseHTTP> deleteTrainingTable(@PathVariable String idTrainingTable) {
-
-        try {
-            trainingTableService.deleteTrainingTable(idTrainingTable);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), idTrainingTable, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idTrainingTable, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @DeleteMapping(Literals.GYM_MACHINE_ID)
-    public ResponseEntity<ResponseHTTP> deleteGymMachine(@PathVariable String idGymMachine) {
-
-        try {
-            gymMachineService.deleteGymMachine(idGymMachine);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), idGymMachine, null);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-        } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idGymMachine, e.getMessage());
             return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
