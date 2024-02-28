0d7a0a5cee600c0f89829fd374bdd2293fcc3c9c
fdeoses <frdeosesfuentelu@gmail.com>
2024-02-28 21:38:58 +0100

    add new url

--- user-server/src/main/java/com/uma/gymfit/user/utils/Literals.java
+++ user-server/src/main/java/com/uma/gymfit/user/utils/Literals.java
@@ -4,6 +4,7 @@ public class Literals {
     public static final String API = "/api/gymfit";
     public static final String USER = "/user";
     public static final String USERS = "/users";
+    public static final String USERS_ROLE_USER = "/users/role/users";
     public static final String USER_ID = "/users/{idUser}";
     public static final String GENERATE_TOKEN = "/generate-token";
     public static final String CURRENT_USER = "/current-user";
