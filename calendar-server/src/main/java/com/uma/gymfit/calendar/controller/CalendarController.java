fcf9384566a9e34445839f1260935d9505fe12b7
fdeoses <frdeosesfuentelu@gmail.com>
2024-02-28 21:43:30 +0100

    Update ResponseHTTP

--- calendar-server/src/main/java/com/uma/gymfit/calendar/controller/CalendarController.java
+++ calendar-server/src/main/java/com/uma/gymfit/calendar/controller/CalendarController.java
@@ -8,8 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,69 +31,68 @@ public class CalendarController {
     private ICalendarService calendarService;
 
     @GetMapping(Literals.CALENDARS)
-    public ResponseEntity<List<Calendar>> allCalendars() {
-        List<Calendar> allCalendar = calendarService.allCalendars();
-        return new ResponseEntity<>(allCalendar, HttpStatus.OK);
+    public ResponseEntity<ResponseHTTP<List<Calendar>>> allCalendars() {
+
+        return ResponseEntity.ok(createResponseHttp(HttpStatus.OK, calendarService.allCalendars(), null));
     }
 
     @GetMapping(Literals.CALENDARS_PUBLISHED)
-    public ResponseEntity<List<Calendar>> allCalendarsPublished() {
-        List<Calendar> allCalendarPublished = calendarService.allCalendarsPublishedActive();
-        return new ResponseEntity<>(allCalendarPublished, HttpStatus.OK);
+    public ResponseEntity<ResponseHTTP<List<Calendar>>> allCalendarsPublished() {
+        return ResponseEntity.ok(createResponseHttp(HttpStatus.OK, calendarService.allCalendarsPublishedActive(), null));
     }
 
     @GetMapping(Literals.CALENDAR_ID)
-    public ResponseEntity<Calendar> findCalendar(@PathVariable String idCalendar) {
-        Calendar calendar;
+    public ResponseEntity<ResponseHTTP<?>> findCalendar(@PathVariable String idCalendar) {
         try {
-            calendar = calendarService.findCalendar(idCalendar);
-            return new ResponseEntity(calendar, HttpStatus.OK);
+            Calendar calendar = calendarService.findCalendar(idCalendar);
+            return ResponseEntity.ok(createResponseHttp(HttpStatus.OK, calendar, null));
         } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idCalendar, e.getMessage());
-            return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().body(createResponseHttp(HttpStatus.INTERNAL_SERVER_ERROR, idCalendar, e.getMessage()));
         }
     }
 
     @PostMapping(Literals.CALENDAR)
-    public ResponseEntity<ResponseHTTP> createCalendar(@Validated @RequestBody Calendar calendar) {
+    public ResponseEntity<ResponseHTTP<Calendar>> createCalendar(@Validated @RequestBody Calendar calendar) {
 
         try {
             calendarService.createCalendar(calendar);
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.CREATED.value(), HttpStatus.CREATED.toString(), calendar, null);
-            return new ResponseEntity<>(res, HttpStatus.CREATED);
+            return ResponseEntity.created(URI.create(Literals.CALENDAR)).body(createResponseHttp(HttpStatus.CREATED, calendar, null));
         } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), calendar, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().body(createResponseHttp(HttpStatus.INTERNAL_SERVER_ERROR, calendar, e.getMessage()));
         }
 
     }
 
 
     @PatchMapping(Literals.CALENDAR)
-    public ResponseEntity<Calendar> updateCalendar(@RequestBody Calendar calendar) {
+    public ResponseEntity<ResponseHTTP<Calendar>> updateCalendar(@RequestBody Calendar calendar) {
 
         try {
-            Calendar calendarUpdate = calendarService.updateCalendar(calendar);
-            return new ResponseEntity<>(calendarUpdate, HttpStatus.OK);
+
+            Calendar calendarSave = calendarService.updateCalendar(calendar);
+
+            return ResponseEntity.ok(createResponseHttp(HttpStatus.OK, calendarSave, null));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(calendar);
+            return ResponseEntity.internalServerError().body(createResponseHttp(HttpStatus.INTERNAL_SERVER_ERROR, calendar, e.getMessage()));
         }
 
     }
 
 
     @DeleteMapping(Literals.CALENDAR_ID)
-    public ResponseEntity<ResponseHTTP> deleteCalendar(@PathVariable String idCalendar) {
+    public ResponseEntity<ResponseHTTP<String>> deleteCalendar(@PathVariable String idCalendar) {
 
         try {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.OK.value(), HttpStatus.OK.toString(), idCalendar, null);
             calendarService.deleteCalendar(idCalendar);
-            return new ResponseEntity<>(res, HttpStatus.OK);
+            return ResponseEntity.ok(createResponseHttp(HttpStatus.OK, idCalendar, null));
         } catch (Exception e) {
-            ResponseHTTP res = new ResponseHTTP(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), idCalendar, e.getMessage());
-            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().body(createResponseHttp(HttpStatus.INTERNAL_SERVER_ERROR, idCalendar, e.getMessage()));
         }
 
+    }
+
+    private <T> ResponseHTTP<T> createResponseHttp(HttpStatus httpStatus, T object, String message) {
+        return new ResponseHTTP<>(httpStatus.value(), httpStatus.toString(), object, message);
     }
 
 
--- calendar-server/src/main/java/com/uma/gymfit/calendar/model/calendar/ResponseHTTP.java
+++ calendar-server/src/main/java/com/uma/gymfit/calendar/model/calendar/ResponseHTTP.java
@@ -1,19 +1,23 @@
 package com.uma.gymfit.calendar.model.calendar;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseHTTP {
+public class ResponseHTTP<T> {
 
     private int code;
 
     private String response;
 
-    private Object body;
+    private T body;
 
     private String error;
 
