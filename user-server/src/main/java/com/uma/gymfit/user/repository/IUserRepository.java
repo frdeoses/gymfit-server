209f2e975d751469d98bc0e1103aa015c702a2b6
fdeoses <frdeosesfuentelu@gmail.com>
2024-05-23 17:30:27 +0200

    Update repository

--- user-server/src/main/java/com/uma/gymfit/user/repository/IUserRepository.java
+++ user-server/src/main/java/com/uma/gymfit/user/repository/IUserRepository.java
@@ -2,12 +2,18 @@ package com.uma.gymfit.user.repository;
 
 import com.uma.gymfit.user.model.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface IUserRepository extends MongoRepository<User, String> {
+
+    @Query("{ 'userRoles.nameRole': ?0 }")
+    List<User> findByUserRoles_Name(String roleName);
 
     User findByUsername(String username);
 
     boolean existsUserByUsername(String username);
 
-    boolean existsUserByEmail(String email);
+    boolean existsUserByPersonalData_Email(String email);
 }
