f83a68b3fb6a388f962b445faee5879b84d41edb
fdeoses <frdeosesfuentelu@gmail.com>
2024-06-04 18:59:42 +0200

    Add constructor.

--- training-table-server/src/main/java/com/uma/gymfit/trainingtable/utils/Literals.java
+++ training-table-server/src/main/java/com/uma/gymfit/trainingtable/utils/Literals.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public class Literals {
 
+    private Literals() {
+        throw new UnsupportedOperationException("Esta es una clase de utilidad y no puede ser instanciada.");
+    }
+
     public static final String API = "/api/gymfit";
 
     public static final String TRAINING_TABLES = "/training-tables";
@@ -37,9 +41,17 @@ public class Literals {
 
     public static final String TRAINING = "/training-tables/training";
 
+    public static final String WORKED_WEIGHTS = "/training-tables/worked-weights";
+
     public static final String GENERATE_TOKEN = "/api/gymfit/training-tables/generate-token";
 
     public static final String CURRENT_USER = "/current-user";
+
+    public static final String NOT_FOUND_TRAINING_MSG = "No se encuentra el entrenamiento con ID: {}";
+
+    public static final String UPDATE_TRAINING_MSG = "Entrenamiento con ID: {} actualizado con éxito.";
+
+    public static final String NOT_FOUND_SYSTEM = " no se encuentra en el sistema.";
 
     public static <T> ResponseHTTP<T> createResponseHttp(HttpStatus httpStatus, T object, String message) {
         return new ResponseHTTP<>(httpStatus.value(), httpStatus.toString(), object, message);
